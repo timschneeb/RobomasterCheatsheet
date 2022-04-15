260783d11f048d46375b8c436cb388e7ced434fb
Tim Schneeberger <tim.schneeberger@outlook.de>
2022-04-15 19:54:34 +0200

    Update gripper.py

--- gripper.py
+++ gripper.py
@@ -3,13 +3,16 @@ from robomaster import robot
 
 ep_robot = robot.Robot()
 
+# Funktion, welche den Gripper öffnet/schließt und den Arm dabei um x & y bewegt
 def move_gripper(robot, x, y, open):
     arm = robot.robotic_arm
     
     time.sleep(1)
+    # Arm bewegen
     arm.move(x, 0).wait_for_completed()
     arm.move(0, -y).wait_for_completed()
 
+    # Gripper öffnen/schließen
     if open: 
         robot.gripper.open(power=50)
     else:
@@ -18,9 +21,11 @@ def move_gripper(robot, x, y, open):
     time.sleep(1)
     robot.gripper.pause()
 
+    # Arm zurück bewegen
     arm.move(0, y).wait_for_completed()
     arm.move(-x, 0).wait_for_completed()    
 
+# Funktion, um den Arm und Gripper in Eingangsposition zu bewegen
 def reset_gripper(robot):
     robot.gripper.open(power=50)
     time.sleep(1)
@@ -36,9 +41,13 @@ if __name__ == '__main__':
 
     reset_gripper(ep_robot)
 
+    # 2m nach vorne fahren
     chassis.move(2, 0, 0, xy_speed=1).wait_for_completed()
+    # Gegenstand greifen
     move_gripper(ep_robot, 50, 50, False)
+    # 2m nach hinten fahren
     chassis.move(-2, 0, 0, xy_speed=1).wait_for_completed()
+    # Gegenstand loslassen
     move_gripper(ep_robot, 50, 50, True)
 
     ep_robot.close()
